4e77a2d3bc5db7883243eadd3b1db246ffb510bc
Inna Mon <inna.mon@gmail.com>
2019-08-02 15:20:32 -0500

    added AUxillary component to wrap Person component data whcih in essence allows us to avoid wrapping adjacent elements in one div and still allows Aux component to spit out single input through the use of props.children

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
-import classes from './Person.module.css';
+import Aux from "./../../hoc/Auxillary";
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>
-                I am {this.props.name} and I am {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-                <input type="text" 
+            <Aux>
+                <p 
+                onClick={this.props.click}>
+                I am {this.props.name} and I am {this.props.age} years old!
+                </p>
+
+                <p>
+                {this.props.children}
+                </p>
+
+                <input 
+                type="text" 
                 onChange={this.props.changed} 
-                value={this.props.name} />
-            </div>
+                value={this.props.name} 
+                />
+            </Aux>
         );
     }
 }
 
 export default Person;
+
+
+
+// import classes from './Person.module.css';
+
+// <div className={classes.Person}>
+// </div>
