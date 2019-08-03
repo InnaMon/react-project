0b499c30fbed39aeac97cf917a6a4c1459ace556
Inna Mon <inna.mon@gmail.com>
2019-08-03 10:46:06 -0500

    added refs property onto the Persons component to focus elements the React way

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -6,6 +6,16 @@ import withClass from './../../hoc/withClass';
 import classes from './Person.module.css';
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering...');
         return (
@@ -21,6 +31,8 @@ class Person extends Component {
 
                 <input 
                 type="text" 
+                // ref={(inputEl) => {this.inputElement = inputEl}}
+                ref={this.inputElementRef}
                 onChange={this.props.changed} 
                 value={this.props.name} 
                 />
