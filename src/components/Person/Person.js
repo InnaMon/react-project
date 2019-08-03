a456b3e55e13997079996c9d2a71ce9c698bb140
Inna Mon <inna.mon@gmail.com>
2019-08-03 10:16:50 -0500

    added PropTypes to Person component that ensures that the correct data types are passed into each props input

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import Aux from './../../hoc/Auxillary';
 import withClass from './../../hoc/withClass';
 import classes from './Person.module.css';
@@ -26,6 +28,13 @@ class Person extends Component {
         );
     }
 }
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
 
